db516df67fe433b71597e56a3f8eda3395e1ca9d
marc <marc@instructure.com>
2014-07-07 10:54:24 -0600

    Setting up the first couple of files to get the automation suite up and running

---
+++ common_spec.rb
@@ -0,0 +1,6 @@
+#This file is to setup the common setup files and methods to start up the automation for gallery
+#
+#Need a method that will create a new instant user that is fully qualified to log into canvas, will need
+#set this up in both canvas and gallery
+#
+#Need a method that will create courses in both canvas and gallery. 
---
+++ index_page_spec.rb
@@ -0,0 +1,20 @@
+#This is an automation script that will automatically pass or fail a test case on testrail
+#
+#Need to make a spec for the following cases
+#
+#it "should show courses when gallery index page is loaded" do
+#  #should have some method in common.rb that seeds courses into the database
+#  #and then should have some verification that says the courses were found.
+#end
+#
+#it "should show Login link in upper right corner when not logged in" do
+#  #should have some method in common.rb that seeds a new user and visits
+#  #the index page
+#end
+#
+#it "should show all settings link when logged in" do
+#  #should have some method in common.rb that seeds a new user and is fully 
+#  #added to canvas, then logs in and visits the index page. 
+#end
+#
+#
